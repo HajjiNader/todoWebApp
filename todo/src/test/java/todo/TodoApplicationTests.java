83e9ce39aea3a7e6b233c028c2caa925174425eb
ADEO\20102220 <nader.hajji@ext.adeo.com>
2022-05-11 22:09:25 +0200

    enc

--- todo/src/test/java/todo/TodoApplicationTests.java
+++ todo/src/test/java/todo/TodoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TodoApplicationTests {
+class TodoApplicationTests { nader test
 
 	@Test
 	void contextLoads() {
